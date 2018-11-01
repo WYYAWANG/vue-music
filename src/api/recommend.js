38a36740c17b19c143852e7860d2972db6fb5666
unknown <1534995542@qq.com>
2018-11-01 17:51:31 +0800

    some little code

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,6 +1,15 @@
 import jsonp from 'common/js/jsonp'
-import { url } from 'inspector';
+import {commonParams, options} from './config'
 
-export function getRecommend(){
-    const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+// 封装的这个方法就是利用jsonp抓取数据
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+    // Object.assign()用于合并对象，返回一个新的对象
+  const data = Object.assign({},commonParams,{
+        platform: 'h5',
+        uni: 0,
+        needNewCode: 1
+    })
+
+    return jsonp(url,data,options)
 }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1>recommend</h1>
+    <div class="recommend-content">
+        <div class="slider-wrap"></div>
+        <div class="recommend-list">
+            <h1 class="list-title"></h1>
+            <ul></ul>
+        </div>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+
+export default {
+    created() {
+        this._getRecommend()
+    },
+    methods: {
+        _getRecommend(){
+            getRecommend().then((res)=>{
+            if(res.code===ERR_OK){
+                console.log(res.data.slider)
+            }   
+            })
+        }
+    }       
+}
 </script>
 
 <style lang="stylus" scoped>
