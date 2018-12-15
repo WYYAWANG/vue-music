f428521a841a24dc5f74cc613042d71ad9b94c0f
Tsubame <1534995542@qq.com>
2018-12-15 22:07:04 +0800

    scroll

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrappper">
-        <slider>
-          <!-- slot -->
-          <div v-for="item of recommends" :key="item.key">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" :key="item.key" class="item">
-            <div class="icon">
-              <img :src="item.imgurl" width="60" height="60">
+    <Scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrappper">
+          <slider>
+            <!-- slot -->
+            <div v-for="item of recommends">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" :load="loadImage">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from "base/scroll/scroll";
 import Slider from "base/slider/slider";
 import { getRecommend, getDiscList } from "api/recommend";
 import { ERR_OK } from "api/config";
@@ -58,12 +61,19 @@ export default {
       getDiscList().then(res => {
         //console.log(res.data)
         this.discList = res.data.list;
-        console.log(this.discList);
+        //console.log(this.discList);
       });
+    },
+    loadImage() {
+      if (!this.checked) {
+        this.$refs.scroll.refresh();
+        this.checked = true;
+      }
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 };
 </script>
